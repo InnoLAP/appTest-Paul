f32cca19fa22790853eb2913455f4a364f23bf9d
Paul Sohns <paul.sohns@web.de>
2022-06-20 22:24:13 +0200

    Prepare project using eslint and style-loader

--- src/test.js
+++ src/test.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import styles from './src/css/global.scss';
+import styles from 'src/css/global.scss';
 
 export default function App() {
-
 	return (
-		<View style={styles.container}>
-			<Text>Open upawd App.js to stahgvrt working on your app!</Text>
+		<View>
+			<Text style={styles.testStyle}>Open up App.js to stahgvrt working on your app!</Text>
 			<StatusBar />
 		</View>
 	);
