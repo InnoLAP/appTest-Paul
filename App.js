55920110495c3f0b5a424223f9e75bd70ebae130
Paul Sohns <paul.sohns@web.de>
2022-06-22 10:13:01 +0200

    Cleanup

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import styles from './src/css/_app.scss';
@@ -6,7 +7,7 @@ export default function App() {
 
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<Text>Hello!</Text>
 			<StatusBar />
 		</View>
 	);
